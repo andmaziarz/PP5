212689b5c7da658a5abbccc111fef4b272be077c
andmaziarz <a.maziarz9@outlook.com>
2023-11-14 17:49:57 +0100

    supplier

--- src/supplier.ts
+++ src/supplier.ts
@@ -1,4 +1,5 @@
 class Supplier extends Customer {
+    invoices: Invoice[];
     accountNumber: number;
     bills: any[]; // Tutaj można również użyć bardziej konkretnego typu, np. Bill[] jeśli jest taki typ zdefiniowany
 
@@ -9,5 +10,6 @@ class Supplier extends Customer {
         super(); // Jeśli klasa Customer ma swój konstruktor, konieczne jest wywołanie super()
         this.accountNumber = accountNumber;
         this.bills = [];
+        this.invoices = [];
     }
 }
